3a4ffb844387eddd83ef4a50452614d0b6f8f794
zupa <thalter@zupa.hu>
2014-11-20 14:41:49 +0000

    remove obsolete methods

--- Instance.Decoratef.go
+++
@@ -1,8 +0,0 @@
-
-package errors
-
-// Decoratef is to Decorate what fmt.Printf is to fmt.Print.
-func (instance *Instance) Decoratef(template string, args ...interface{}) (Error) {
-	return instance
-}
-
